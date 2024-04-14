8c33ded0fcb75fac32208b3892c2a530825a34a5
Cristian Ruiz Martín <cristianruizm@uma.es>
2024-04-14 22:24:27 +0200

    minor fixes

--- P4/funciones_aes.py
+++ P4/funciones_aes.py
@@ -5,20 +5,6 @@ def crear_AESKey():
     """Devuelve un número aleatorio de 16 bytes - 128 bits"""
     return get_random_bytes(16)
 
-<<<<<<< HEAD
-"""
-En esta librería, AES GCM funciona de la misma forma para el emisor y para el receptor (se utiliza iniciarAES_GCM). 
-Esto es así porque las entidades únicamente se intercambian un mensaje.
-Sin embargo, si tratásemos de hacer el paso 7 con esta forma de hacer las cosas, ¡tendríamos un error!
-¿Por qué? Revisa la documentación de pycryptodome y mira que es realmente el parámetro aes_cifrado.nonce...
-"""
-
-def iniciarAES_GCM(key_16):
-    """Inicia el engine de cifrado AES CTR"""
-    nonce_16 = get_random_bytes(16)
-    aes_cifrado = AES.new(key_16, AES.MODE_GCM, nonce = nonce_16, mac_len = 16)
-    return aes_cifrado
-=======
 def iniciarAES_GCM_cifrado(key_16):
     """Inicia el engine de cifrado AES CTR"""
     nonce_16_ini = get_random_bytes(16)
@@ -29,40 +15,22 @@ def iniciarAES_GCM_descifrado(key_16, nonce_16_ini):
     """Inicia el engine de descifrado AES CTR"""
     aes_descifrado = AES.new(key_16, AES.MODE_GCM, nonce = nonce_16_ini, mac_len = 16)
     return aes_descifrado
->>>>>>> 05df069274a2e161a8eb5b611a1e2245ef4c58d1
 
 def cifrarAES_GCM(aes_cifrado, datos):
     """Cifra el parametro datos (de tipo array de bytes), y devuelve el texto cifrado binario Y el mac Y el nonce """
     # En escenarios reales, el objeto AES se inicializa una vez, y luego vamos llamando a aes_cifrado
     datos_cifrado, mac_cifrado = aes_cifrado.encrypt_and_digest(datos)
-<<<<<<< HEAD
-    return datos_cifrado, mac_cifrado, aes_cifrado.nonce
-
-def descifrarAES_GCM(key_16, nonce_16, datos, mac):
-    """Descifra el parametro datos (de tipo binario), y devuelve los datos descrifrados de tipo array de bytes.
-       También comprueba si el mac es correcto"""
-    try:
-        aes_descifrado = AES.new(key_16, AES.MODE_GCM, nonce = nonce_16)
-=======
     return datos_cifrado, mac_cifrado
 
 def descifrarAES_GCM(aes_descifrado, datos, mac):
     """Descifra el parametro datos (de tipo binario), y devuelve los datos descrifrados de tipo array de bytes.
        También comprueba si el mac es correcto"""
     try:
->>>>>>> 05df069274a2e161a8eb5b611a1e2245ef4c58d1
         datos_claro = aes_descifrado.decrypt_and_verify(datos, mac)
         return datos_claro
     except (ValueError, KeyError) as e:
         return False
         
-<<<<<<< HEAD
-"""
-Para permitir que se envíen más mensajes en un mismo canal, para los pasos 5-7 utilizaremos esta API.
-"""
-
-=======
->>>>>>> 05df069274a2e161a8eb5b611a1e2245ef4c58d1
 def iniciarAES_CTR_cifrado(key_16):
     """Inicia el engine de cifrado AES CTR"""
     nonce_16_ini = get_random_bytes(8) # nonce aleatorio de 64 bits
